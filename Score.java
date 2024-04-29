d9d42c9fdf1169faa1967581309eb6b7e4be6e8f
Muntahi-Safwan <muntahi1212@gmail.com>
2024-04-30 01:36:45 +0600

    Result page completed

--- Score.java
+++ Score.java
@@ -11,7 +11,7 @@ public class Score extends JFrame implements ActionListener {
         this.setResizable(false);
         this.setLocationRelativeTo(null);
         
-        ImageIcon selectImg = new ImageIcon("./assets/quiz.png");
+        ImageIcon selectImg = new ImageIcon("./assets/score.png");
         ImageIcon BackgroundImg = new ImageIcon("./assets/background-7.png");
         ImageIcon blur = new ImageIcon("./assets/Rectangle-21.png");
 
@@ -22,28 +22,53 @@ public class Score extends JFrame implements ActionListener {
         blurLabel.setIcon(blur);
         blurLabel.setBounds(35, 45, 940, 580);
 
-        
         JLabel select = new JLabel();
         select.setIcon(selectImg);
-        select.setBounds(270, 40, 400, 278);
+        select.setBounds(362, 70, 300, 300);
         
-        JLabel heading = new JLabel("Thankyou " + name + " for playing Simple Minds");
-        heading.setBounds(45, 30, 700, 30);
-        heading.setFont(new Font("Tahoma", Font.PLAIN, 26));
+        JLabel heading = new JLabel("Submitted Successfully!!!");
+        heading.setBounds(337, 360, 380, 30);
+        heading.setFont(new Font("Roboto", Font.BOLD, 30));
+        heading.setVerticalTextPosition(JLabel.CENTER);
+        heading.setHorizontalTextPosition(JLabel.CENTER);
+        heading.setForeground(Color.black);
         add(heading);
+
+        JLabel l1 = new JLabel("Name: " + name);
+        l1.setBounds(351, 405, 336, 30);
+        l1.setFont(new Font("Roboto", Font.PLAIN, 25));
+        l1.setVerticalTextPosition(JLabel.CENTER);
+        l1.setHorizontalTextPosition(JLabel.CENTER);
+        l1.setForeground(Color.black);
+        add(l1);
         
-        JLabel lblscore = new JLabel("Your score is " + score);
-        lblscore.setBounds(350, 200, 300, 30);
-        lblscore.setFont(new Font("Tahoma", Font.PLAIN, 26));
+        JLabel lblscore = new JLabel("Obtained Mark: " + score);
+        lblscore.setBounds(351, 449, 336, 30);
+        lblscore.setFont(new Font("Tahoma", Font.PLAIN, 25));
+        lblscore.setVerticalTextPosition(JLabel.CENTER);
+        lblscore.setHorizontalTextPosition(JLabel.CENTER);
+        lblscore.setForeground(Color.black);
         add(lblscore);
+
+        int incorrect = (100 - score) / 10;
+
+        JLabel l2 = new JLabel("Incorrect: " + incorrect);
+        l2.setBounds(351, 493, 336, 30);
+        l2.setFont(new Font("Tahoma", Font.PLAIN, 25));
+        l2.setVerticalTextPosition(JLabel.CENTER);
+        l2.setHorizontalTextPosition(JLabel.CENTER);
+        l2.setForeground(Color.black);
+        add(l2);
         
-        JButton submit = new JButton("Play Again");
-        submit.setBounds(380, 270, 120, 30);
-        submit.setBackground(new Color(30, 144, 255));
-        submit.setForeground(Color.WHITE);
+        JButton submit = new JButton("Next");
+        submit.setBounds(400, 540, 226, 50);
+        submit.setBackground(new Color(0xB8E4FA));
+        submit.setFont(new Font("Roboto", Font.BOLD, 32));
+        submit.setForeground(Color.BLACK);
         submit.addActionListener(this);
         add(submit);
         
+        this.add(select);
         this.add(blurLabel);
         this.add(background);
         setVisible(true);
@@ -51,10 +76,11 @@ public class Score extends JFrame implements ActionListener {
     
     public void actionPerformed(ActionEvent ae) {
         setVisible(false);
-        new Login();
+        SelectCourse s = new SelectCourse();
+        s.setVisible(true);
     }
 
     public static void main(String[] args) {
-        new Score("User", 0);
+        new Score("Jawan", 60);
     }
 }
